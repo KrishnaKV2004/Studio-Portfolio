c798acce126b9b4c2d829455316d6b130bef6eba
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2025-08-05 23:47:31 +0530

    Initial Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+  <div className="container mx-auto max-w-7xl">
+    <Navbar />
+    <Hero />
+    {/*About*/}
+    {/*Projects*/}
+    {/*Experience*/}
+    {/*Testimonial*/}
+    {/*Contact*/}
+    {/*Footer*/}
+  </div>
+  )
+}
+
+export default App;
