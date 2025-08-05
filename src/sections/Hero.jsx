2af2b07e8100f99acfd455125d785f5c22e28e79
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2025-08-06 00:15:45 +0530

    Add Parallax Scroll

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,11 @@
 import HeroText from "../components/HeroText";
+import Parallax from "../components/Parallax";
 
 const Hero = () => {
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText />
+        <Parallax />
     </section>
   );
 }
